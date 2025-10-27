ef92025d63f17aa0ebfee57948d3079dd80b5074
Julien Richoz <julienrichoz@outlook.com>
2025-10-27 11:00:16 +0100

    Modify router

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
+// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import CovariatesPage from './pages/CovariatesPage'
 import './styles.css'
 
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/covariates', element: <CovariatesPage /> },
-])
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/covariates" element={<CovariatesPage />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
